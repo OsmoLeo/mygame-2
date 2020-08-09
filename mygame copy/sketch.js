47085ac0a13628c7d47b0aa184c2144cd71fe502
borntosk8 <59756311+borntosk8@users.noreply.github.com>
2020-08-09 22:04:22 +0530

    Add files via upload

---
+++ mygame copy/sketch.js
@@ -0,0 +1,113 @@
+var ninja
+var ground
+var diesound;
+var jumpsound;
+var gameState = 'play';
+var score=0;
+
+
+function preload(){
+  bg=loadImage("woods.jpg");
+  ninja_img=loadAnimation("ninja1.png","ninja2.png","ninja3.png","ninja4.png")
+  stone_img=loadImage("stone.png")
+  diesound=loadSound("diesound.mp3")
+  jumpsound=loadSound("jumpsound.mp3")
+  fireball_img=loadAnimation("fireball1.png","fireball2.png","fireball3.png","fireball4.png")
+  ninjacoin_img=loadImage("ninja coin.png")
+  
+}
+  
+
+function setup() {
+  createCanvas(1700,400);
+  ninja=createSprite(250,270,20,20)
+  ninja.addAnimation("ninja",ninja_img);
+  ninja.scale=0.5;
+  ground=createSprite(850,380,1400,10);
+}
+
+
+function draw() {
+  background(bg);
+  if(keyDown('space')){
+    fireball();
+  }
+  
+  if(gameState === 'play' ){
+    
+    bg.velocityX=-10;
+    stone();
+    ninjacoin();
+    if(keyDown(UP_ARROW) && ninja.y > 203 ){
+      ninja.velocityY=-10;
+      jumpsound.play()
+    }
+      
+
+
+      ninja.velocityY=ninja.velocityY+0.5; 
+      ninja.collide(ground);
+     if(ninja.x-stone.x < ninja.width/2 + stone.width/2
+      && stone.x- ninja.x< ninja.width/2 + stone.width/2
+      && ninja.y -  stone.y < ninja.height/2 + stone.height/2
+      && stone.y - ninja.y < ninja.height/2 + stone.height/2){
+        gameState ==='end';
+        diesound.play()  
+     }
+  
+     
+  }
+  if(gameState === 'end'){
+    ninja.velocityX=0;
+    stone.velocityX=0;
+    ninja.velocityY=0;
+    stone.velocityY=0;
+
+  }
+   
+
+  ground.visible=false;
+  
+  drawSprites();
+  textSize(22);
+  text('SCORE'+score,800,20)
+  
+}
+ 
+
+
+function stone(){
+  if(World.frameCount%100===0){
+  var stone=createSprite(1600,330,20,20)
+  stone.addImage("stone",stone_img);
+  stone.scale=0.3;
+  stone.velocityX=-8;
+  stone.lifetime = 195;
+  }
+}
+  
+
+function fireball(){
+  if(World.frameCount%100===0){
+    var fireball=createSprite(150,270,10,10)
+    fireball.addAnimation("fireball",fireball_img)
+    fireball.velocityX=10;
+    fireball.lifetime = 135;
+  }
+}
+/*funciton ninjacoin (){
+  if(World.frameCount%100===0){
+  var ninjacoin=createSprite(1600,330,10,10)
+  ninjacoin.addImage()
+  ninjacoin.velocityX=5;
+}
+}*/
+function ninjacoin (){
+  if(World.frameCount%100===0){
+    var ninjacoin=createSprite(1600,270,10,10)
+    ninjacoin.addImage("ninjacoin",ninjacoin_img)
+    ninjacoin.velocityX=-25;
+    ninjacoin.scale=-0.2
+    ninjacoin.lifetime = 55
+  }
+}
